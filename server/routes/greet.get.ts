9584f161dda946d142120df6649117e89c9239ef
dvogelg4al <danny.hj.vogel@gmail.com>
2024-05-30 23:45:37 +0200

    Fix: add response headers

--- server/routes/greet.get.ts
+++ server/routes/greet.get.ts
@@ -1,3 +1,10 @@
 export default defineEventHandler(async (event) => {
+  setResponseHeaders(event, {
+    "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Expose-Headers": "*",
+  });
   return "Hello from Danny's rest api!";
 });
