a8833e549924b7675bda67e56f7b9ecf875310df
dvogelg4al <danny.hj.vogel@gmail.com>
2024-05-30 23:59:59 +0200

    Fix: update headers

--- server/routes/greet.get.ts
+++ server/routes/greet.get.ts
@@ -9,7 +9,7 @@ export default defineEventHandler(async (event) => {
     "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Expose-Headers": "*",
   });
   const authHeader = getRequestHeader(event, "Authorization");
