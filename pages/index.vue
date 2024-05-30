401dbe3b7441f2cb43b683df724248203802bb71
dvogelg4al <danny.hj.vogel@gmail.com>
2024-05-30 23:31:46 +0200

    Feat: init greet server route

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const message = ref<string>("");
+const greet = async () => {
+  try {
+    message.value = await $fetch("/greet");
+  } catch (error) {}
+};
+</script>
+
+<template>
+  <div class="flex flex-col gap-4 items-start">
+    <h1 class="text-blue-500 text-5xl">hello</h1>
+    <UButton @click="greet">Get a greeting</UButton>
+    <p class="text-green-500">{{ message }}</p>
+  </div>
+</template>
---
+++ server/routes/greet.get.ts
@@ -0,0 +1,3 @@
+export default defineEventHandler(async (event) => {
+  return "Hello from Danny's rest api!";
+});
