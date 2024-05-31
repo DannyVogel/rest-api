1deec23e3da43c057fa6d384e210d3e1a4e297ea
dvogelg4al <danny.hj.vogel@gmail.com>
2024-05-31 23:55:13 +0200

    Style: add style to scrollbars

--- app.vue
+++ app.vue
@@ -3,3 +3,23 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+  background-color: transparent !important;
+}
+
+::-webkit-scrollbar-track {
+  border-radius: 1rem;
+  border-left: 1px solid rgba(211, 211, 211, 0);
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 20px;
+  background: linear-gradient(30deg, gray, gray);
+  box-shadow: inset 0 0 6px gray;
+  cursor: pointer;
+}
+</style>
