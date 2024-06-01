22f84e9b91e841eb5b720f9a1c3ce58e7a1e53fa
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-02 00:24:17 +0200

    Refactor: simplify and setResponseStatus for 2xx codes

--- server/api/errors/[code].get.ts
+++ server/api/errors/[code].get.ts
@@ -1,55 +1,12 @@
-import type { Response, ErrorCodeLibrary } from "~/types/common.interfaces";
+import type { Response } from "~/types/common.interfaces";
+import { errorCodeLibrary } from "~/server/utils/errors";
 
-const errorCodeLibrary: ErrorCodeLibrary = {
-  200: {
-    statusCode: 200,
-    message: "All good",
-  },
-  201: {
-    statusCode: 201,
-    message: "Welcome to Creation",
-  },
-  202: {
-    statusCode: 202,
-    message: "Acceptable...",
-  },
-  204: {
-    statusCode: 204,
-    message: "Got nothing for you",
-  },
-  400: {
-    statusCode: 400,
-    message: "Because you're bad at requesting",
-  },
-  401: {
-    statusCode: 401,
-    message: "Uh uh, you don't belong here",
-  },
-  403: {
-    statusCode: 403,
-    message: "Definitely not allowed here",
-  },
-  404: {
-    statusCode: 404,
-    message: "Nothing to see here",
-  },
-  500: {
-    statusCode: 500,
-    message: "Everything is broken",
-  },
-};
-
-export default defineEventHandler(async (event) => {
-  const response = {} as Response;
+export default defineEventHandler(async (event): Promise<Response> => {
   const code = Number(getRouterParam(event, "code"));
   if (code >= 200 && code < 300) {
-    response.statusCode = code;
-    response.message = errorCodeLibrary[code].message;
-    return response;
+    setResponseStatus(event, code);
+    return serverResponse(code, errorCodeLibrary[code].message);
   } else {
-    throw createError({
-      statusCode: code,
-      statusMessage: errorCodeLibrary[code].message,
-    });
+    return throwError(code);
   }
 });
