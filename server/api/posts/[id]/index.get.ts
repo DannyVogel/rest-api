01d631aa2fbb2f50722df0e2d5ff8f2135e4602a
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-02 00:17:33 +0200

    Refactor: change endpoint and simplify handler

---
+++ server/api/posts/[id]/index.get.ts
@@ -0,0 +1,20 @@
+import type { Response, Post } from "~/types/common.interfaces";
+
+export default defineEventHandler(async (event): Promise<Response> => {
+  const id = getRouterParam(event, "id");
+  if (isNaN(Number(id))) return throwError(400);
+
+  let posts;
+
+  try {
+    posts = (await storage.getItem("posts.json")) as Post[];
+  } catch (error) {
+    return throwError(500);
+  }
+  if (!posts) return throwError(404);
+
+  const post = posts.find((post) => post.post_id === id);
+  if (!post) return throwError(404);
+
+  return serverResponse(200, "Post found", [post]);
+});
