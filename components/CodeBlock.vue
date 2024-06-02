a4866c6f9266d8760986c4346346afed4c65310d
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-03 00:41:48 +0200

    Feat: init CodeBlock component w/ Shiki

---
+++ components/CodeBlock.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+const props = defineProps({
+  code: {
+    type: String,
+    required: true,
+  },
+});
+
+const highlightedJS = await useShikiHighlighted(props.code);
+// const highlightedNuxt = await useShikiHighlighted(nuxtCode);
+
+// const tabs = [
+//   {
+//     label: "javascript",
+//   },
+//   {
+//     label: "nuxt",
+//   },
+// ];
+</script>
+
+<template>
+  <UCard
+    class="w-full border border-primary bg-[#0d1117] last:dark:bg-[#0d1117] overflow-scroll"
+    :ui="{ body: { padding: 'pt-0 sm:pt-0' } }"
+  >
+    <div v-html="highlightedJS" />
+  </UCard>
+
+  <!-- <UTabs :items="tabs" class="w-full">
+    <template #item="{ item }">
+  <UCard
+    class="w-full border border-primary bg-[#0d1117] last:dark:bg-[#0d1117]"
+  >
+    <p class="text-white">Get All Posts: '/api/posts'</p>
+    <div v-html="highlightedJS" />
+    <div v-if="item.label === 'javascript'" v-html="highlightedJS" />
+        <div v-else-if="item.label === 'nuxt'" v-html="highlightedNuxt" />
+  </UCard>
+  </template>
+  </UTabs> -->
+</template>
