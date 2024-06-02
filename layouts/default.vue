7f7b277ebb97886fd5a923bd466d1d992e6742e6
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-03 00:42:29 +0200

    Feat: update layout with navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,51 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { version } from "~/package.json";
+
+const links = [
+  [
+    {
+      label: "Trigger Errors",
+      icon: "i-heroicons-shield-exclamation",
+      to: "/trigger-errors",
+    },
+    {
+      label: "Placeholder Posts",
+      icon: "i-heroicons-document-text",
+      to: "/posts",
+    },
+    {
+      label: "User Posts",
+      icon: "i-heroicons-document-text",
+      to: "",
+    },
+  ],
+  [
+    {
+      label: "Examples",
+      icon: "i-heroicons-light-bulb",
+    },
+    {
+      label: "Help",
+      icon: "i-heroicons-question-mark-circle",
+    },
+  ],
+];
+</script>
 
 <template>
-  <div class="container p-4">
-    <slot />
+  <div class="">
+    <div class="flex items-center gap-4 max-w-screen-lg mx-auto">
+      <NuxtLink to="/">
+        <h1 class="text-blue-500 text-2xl font-bold w-fit whitespace-nowrap">
+          TestREST <span class="text-xs">v{{ version }}</span>
+        </h1>
+      </NuxtLink>
+      <UHorizontalNavigation :links="links" class="" />
+    </div>
+    <div class="max-w-screen-lg mx-auto container p-4 mt-10">
+      <slot />
+    </div>
   </div>
 </template>
 
-<style>
-body {
-  background-color: #464242;
-}
-</style>
+<style></style>
