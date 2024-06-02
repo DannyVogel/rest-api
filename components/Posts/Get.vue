a475929c79c96e50a0bd197e395e3bed1377c7f4
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-03 00:42:16 +0200

    Feat: init PostsGet component

---
+++ components/Posts/Get.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import type { Post } from "~/types/common.interfaces";
+
+const code = `
+const res = await fetch(\`/api/posts/, {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+`;
+const title = "Get All Posts";
+const posts = ref<Post[]>([]);
+const number = ref<number>();
+
+const getPosts = async () => {
+  try {
+    const res = await $fetch(`/api/posts/`, {
+      method: "GET",
+    });
+    if (res.statusCode === 200 && res.payload) {
+      posts.value = res.payload as Post[];
+    } else {
+      posts.value = [];
+    }
+  } catch (error: Response | any) {
+    console.log("error", error);
+    posts.value = [];
+  }
+};
+</script>
+
+<template>
+  <APIBlock :title="title" :code="code" />
+
+  <UButton @click="getPosts()">Get All Posts </UButton>
+  <UCard v-if="posts" class="w-full overflow-x-scroll">
+    <h1 class="font-bold text-xl">Posts</h1>
+    <UTable :rows="posts" class="overflow-y-scroll max-h-64 max-w-full">
+      <template #title-data="{ row }">
+        <p class="max-w-72">{{ row.title }}</p>
+      </template>
+      <template #body-data="{ row }">
+        <p class="max-w-72">{{ row.body }}</p>
+      </template>
+    </UTable>
+  </UCard>
+</template>
