cf4b17fb979733a3ec31845f0db6eaf1ceae6466
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-03 00:42:03 +0200

    Feat: init APIBlock component

---
+++ components/APIBlock.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="w-full flex flex-col items-start gap-4">
+    <h2 class="text-2xl font-bold">{{ title }}</h2>
+    <CodeBlock :code="code" />
+  </div>
+</template>
