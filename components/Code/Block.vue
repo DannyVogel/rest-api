70da7c15029f33b58a5c55b90e7fec9e1a9367ad
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-03 22:50:20 +0200

    Feat: init copy functionality

--- components/Code/Block.vue
+++ components/Code/Block.vue
@@ -7,12 +7,38 @@ const props = defineProps({
 });
 
 const highlightedJS = await useShikiHighlighted(props.code);
+
+const copied = ref(false);
+
+const copyToClipboard = () => {
+  navigator.clipboard.writeText(props.code);
+  copied.value = true;
+  setTimeout(() => {
+    copied.value = false;
+  }, 2000);
+};
 </script>
 
 <template>
   <UCard
-    class="w-full border border-primary bg-[#0d1117] dark:bg-[#0d1117] overflow-scroll"
+    class="relative w-full border border-primary bg-[#0d1117] dark:bg-[#0d1117] overflow-scroll"
   >
     <div v-html="highlightedJS" />
+    <UTooltip
+      text="Copy to clipboard"
+      class="absolute top-2 right-2"
+      @click="copyToClipboard"
+    >
+      <UButton
+        :icon="
+          copied ? 'i-heroicons-check-badge' : 'i-heroicons-clipboard-document'
+        "
+        size="xs"
+        :color="copied ? 'green' : 'blue'"
+        square
+        variant="ghost"
+        class="transition-all duration-300 ease-in-out"
+      />
+    </UTooltip>
   </UCard>
 </template>
