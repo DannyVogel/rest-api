39c3f89f36dd52fe4b3b3280a474c93c49c2a3a1
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-03 22:51:26 +0200

    Fix: use basic fetch api for example code; show actual output; use loading state; add clear button; update design

--- components/Posts/Get.vue
+++ components/Posts/Get.vue
@@ -1,24 +1,28 @@
 <script lang="ts" setup>
 import type { Post } from "~/types/common.interfaces";
 
-const code = `const res = await fetch(\`/api/posts/, {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});`;
-const output = `[
-  { id: 1, author: '...', title: '...', body: '...' },
-  { id: 2, author: '...', title: '...', body: '...' },
-  { id: 3, author: '...', title: '...', body: '...' },
-  /* ... */
-  { id: 100, author: '...', title: '...', body: '...' },
-];`;
+const code = `fetch("https://restfultest.netlify.app/api/posts")
+  .then((response) => response.json())
+  .then((data) => console.log(data));`;
+
+const output = `{
+    "statusCode": 200,
+    "message": "Got all posts",
+    "payload": [
+      { post_id: 1, author: '...', title: '...', body: '...' },
+      { post_id: 2, author: '...', title: '...', body: '...' },
+      { post_id: 3, author: '...', title: '...', body: '...' },
+      /* ... */
+      { post_id: 100, author: '...', title: '...', body: '...' },
+    ]
+};`;
+
 const title = "Get All Posts";
 const posts = ref<Post[]>([]);
-const number = ref<number>();
+const isLoading = ref<boolean>(false);
 
 const getPosts = async () => {
+  isLoading.value = true;
   try {
     const res = await $fetch(`/api/posts/`, {
       method: "GET",
@@ -31,6 +35,8 @@ const getPosts = async () => {
   } catch (error: Response | any) {
     console.log("error", error);
     posts.value = [];
+  } finally {
+    isLoading.value = false;
   }
 };
 </script>
@@ -38,15 +44,35 @@ const getPosts = async () => {
 <template>
   <div class="w-full flex flex-col items-start gap-4">
     <APIBlock :title="title" :code-input="code" :code-output="output" />
-    <UButton @click="getPosts()">Get All Posts </UButton>
-    <UCard v-if="posts" class="w-full overflow-x-scroll">
-      <h1 class="font-bold text-xl">Posts</h1>
+    <p class="font-bold">Live Data Example:</p>
+    <UCard class="w-full overflow-x-scroll">
+      <div class="flex justify-between items-baseline mb-2">
+        <h1 class="font-bold text-xl">Posts</h1>
+        <p
+          v-if="posts.length"
+          @click="posts = []"
+          class="text-sm hover:text-red-500 cursor-pointer"
+        >
+          Clear data
+        </p>
+      </div>
       <UTable :rows="posts" class="overflow-y-scroll max-h-64 max-w-full">
         <template #title-data="{ row }">
           <p class="max-w-72">{{ row.title }}</p>
         </template>
         <template #body-data="{ row }">
           <p class="max-w-72">{{ row.body }}</p>
+        </template>
+        <template #empty-state>
+          <div class="flex items-center justify-center h-60">
+            <UButton
+              size="xl"
+              color="green"
+              :loading="isLoading"
+              @click="getPosts()"
+              >Get All Posts
+            </UButton>
+          </div>
         </template>
       </UTable>
     </UCard>
