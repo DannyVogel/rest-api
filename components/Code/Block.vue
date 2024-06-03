19bb09e64d1ee673b0a0b07bc0b0ca72c285b612
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-03 08:40:44 +0200

    Refactor: move file

---
+++ components/Code/Block.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps({
+  code: {
+    type: String,
+    required: true,
+  },
+});
+
+const highlightedJS = await useShikiHighlighted(props.code);
+</script>
+
+<template>
+  <UCard
+    class="w-full border border-primary bg-[#0d1117] dark:bg-[#0d1117] overflow-scroll"
+  >
+    <div v-html="highlightedJS" />
+  </UCard>
+</template>
