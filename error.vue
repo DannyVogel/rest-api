d2b0411b87a56621de239988aa28a25855caf58b
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-05 21:16:07 +0200

    WIP: init error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <div>
+    <h1>THE ERROR PAGE</h1>
+    <h2>{{ error.statusCode }}</h2>
+    <button @click="handleError">Clear errors</button>
+  </div>
+</template>
