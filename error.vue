1805c79a5eadc3ea7c75d0e394e0200019e0bb52
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-05 21:27:17 +0200

    Fix: update design, add error.message

--- error.vue
+++ error.vue
@@ -10,9 +10,17 @@ console.error("error page", props.error);
 </script>
 
 <template>
-  <div>
-    <h1>THE ERROR PAGE</h1>
-    <h2>{{ error.statusCode }}</h2>
-    <button @click="handleError">Clear errors</button>
+  <div
+    class="w-screen h-screen flex flex-col items-center justify-center gap-4"
+  >
+    <h2 class="text-3xl font-bold">THE ERROR PAGE</h2>
+    <h1 class="text-5xl font-bold">{{ error.statusCode }}</h1>
+    <p class="text-lg">{{ error.message }}</p>
+    <button
+      class="px-2 py-1 bg-green-600 text-white font-bold border border-green-700 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out"
+      @click="handleError"
+    >
+      Clear errors
+    </button>
   </div>
 </template>
