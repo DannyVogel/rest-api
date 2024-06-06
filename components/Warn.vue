94fe12bd7a92ce42f1ca5ced9b81fd4810691b10
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-06 23:24:11 +0200

    Feat: init warning message component

---
+++ components/Warn.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="flex gap-2 items-center">
+    <UIcon
+      name="i-heroicons-exclamation-triangle"
+      class="text-orange-500 mt-1"
+    />
+    <p class="text-white">{{ message }}</p>
+  </div>
+</template>
