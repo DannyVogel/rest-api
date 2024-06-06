5b60f031dee09eeb9e82162b91df9d20c608ffe9
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-06 23:24:52 +0200

    Feat: add body validation to posts/create and handle errors

--- server/api/posts/create.post.ts
+++ server/api/posts/create.post.ts
@@ -1,5 +1,12 @@
+import { postSchema } from "~/server/validator/posts";
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   if (!body) return throwError(400);
-  return serverResponse(200, "Post created", body);
+  // Validate the body against the schema
+  const { error } = postSchema.validate(body);
+  if (error) return throwError(400, error.details[0].message);
+  body.post_id = "101";
+  await waitSeconds(1);
+  return serverResponse(200, "Post created", [body]);
 });
