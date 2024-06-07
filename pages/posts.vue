61626f41a345947279c7d7b4b304f9f48fab7578
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-07 08:25:38 +0200

    Feat: add Update and Delete components

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,39 +1,4 @@
-<script lang="ts" setup>
-import type { Post } from "~/types/common.interfaces";
-
-const createPost = async () => {
-  const post: Post = {
-    author: "John Doe",
-    post_id: "1",
-    title: "Title",
-    body: "Body",
-  };
-  const res = await $fetch("/api/posts/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: post,
-  });
-  console.log("res", res);
-};
-
-const updatePost = async () => {
-  const post = {
-    author: "John Doe",
-    post_id: "1",
-    body: "Body",
-  };
-  const res = await $fetch("/api/posts/1/update", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: post,
-  });
-  console.log("res", res);
-};
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div class="flex flex-col items-start gap-10">
@@ -42,6 +7,10 @@ const updatePost = async () => {
     <PostsGetById />
     <UDivider />
     <PostsCreate />
+    <UDivider />
+    <PostsUpdate />
+    <UDivider />
+    <PostsDelete />
     <UDivider />
   </div>
 </template>
