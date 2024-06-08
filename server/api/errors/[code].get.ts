7075cdfcf3d1672cee5f4a2ce628861c1a991105
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-08 19:23:22 +0200

    Refactor: use only for error codes

--- server/api/errors/[code].get.ts
+++ server/api/errors/[code].get.ts
@@ -1,19 +1,10 @@
 import type { Response } from "~/types/common.interfaces";
-import { errorCodeLibrary } from "~/server/utils/errors";
 
 export default defineEventHandler(async (event): Promise<Response> => {
   const code = Number(getRouterParam(event, "code"));
-  const isCodeInLibrary = Object.keys(errorCodeLibrary).includes(
-    code.toString()
-  );
+  const isCodeInLibrary = Object.keys(httpStatus).includes(code.toString());
   if (!isCodeInLibrary) {
     throw createError({ statusCode: code });
   }
-
-  if (code >= 200 && code < 300) {
-    setResponseStatus(event, code);
-    return serverResponse(code, errorCodeLibrary[code].message);
-  } else {
-    return throwError(code);
-  }
+  return throwError(code);
 });
