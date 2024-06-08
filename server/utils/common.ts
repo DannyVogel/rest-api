28826f4fa326afd305126e6038109e01d2296619
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-08 19:30:20 +0200

    Refactor: move httpStatus to common

--- server/utils/common.ts
+++ server/utils/common.ts
@@ -1,3 +1,5 @@
+import type { HttpStatus } from "~/types";
+
 export const serverResponse = (
   statusCode: number,
   message?: string,
@@ -13,3 +15,54 @@ export const serverResponse = (
 
 export const waitSeconds = async (seconds: number) =>
   new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+
+export const httpStatus: HttpStatus = {
+  200: {
+    statusCode: 200,
+    statusMessage: "OK",
+  },
+  201: {
+    statusCode: 201,
+    statusMessage: "Created",
+  },
+  202: {
+    statusCode: 202,
+    statusMessage: "Accepted",
+  },
+  204: {
+    statusCode: 204,
+    statusMessage: "No Content",
+  },
+  400: {
+    statusCode: 400,
+    statusMessage: "Bad Request",
+  },
+  401: {
+    statusCode: 401,
+    statusMessage: "Unauthorized",
+  },
+  403: {
+    statusCode: 403,
+    statusMessage: "Forbidden",
+  },
+  404: {
+    statusCode: 404,
+    statusMessage: "Not Found",
+  },
+  409: {
+    statusCode: 409,
+    statusMessage: "Conflict",
+  },
+  429: {
+    statusCode: 429,
+    statusMessage: "Too Many Requests",
+  },
+  500: {
+    statusCode: 500,
+    statusMessage: "Internal Server Error",
+  },
+  503: {
+    statusCode: 503,
+    statusMessage: "Service Unavailable",
+  },
+};
--- server/utils/errors.ts
+++ server/utils/errors.ts
@@ -1,58 +1,5 @@
-import type { HttpStatus } from "~/types";
-
 export const throwError = (errorCode: number, message: string = "") => {
   const error = { ...httpStatus[errorCode] };
   if (message) error.message = message;
   throw createError(error);
-};
-
-export const httpStatus: HttpStatus = {
-  200: {
-    statusCode: 200,
-    statusMessage: "OK",
-  },
-  201: {
-    statusCode: 201,
-    statusMessage: "Created",
-  },
-  202: {
-    statusCode: 202,
-    statusMessage: "Accepted",
-  },
-  204: {
-    statusCode: 204,
-    statusMessage: "No Content",
-  },
-  400: {
-    statusCode: 400,
-    statusMessage: "Bad Request",
-  },
-  401: {
-    statusCode: 401,
-    statusMessage: "Unauthorized",
-  },
-  403: {
-    statusCode: 403,
-    statusMessage: "Forbidden",
-  },
-  404: {
-    statusCode: 404,
-    statusMessage: "Not Found",
-  },
-  409: {
-    statusCode: 409,
-    statusMessage: "Conflict",
-  },
-  429: {
-    statusCode: 429,
-    statusMessage: "Too Many Requests",
-  },
-  500: {
-    statusCode: 500,
-    statusMessage: "Internal Server Error",
-  },
-  503: {
-    statusCode: 503,
-    statusMessage: "Service Unavailable",
-  },
 };
