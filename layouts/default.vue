d4cf51fb32c4b1aeeb29b443a6a1caff513652eb
dvogelg4al <danny.hj.vogel@gmail.com>
2024-06-09 16:27:53 +0200

    Style: reduce mt

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="mt-4">
+  <div class="mt-2">
     <TheHeader />
     <div class="max-w-screen-lg mx-auto container p-4 mt-10">
       <slot />
