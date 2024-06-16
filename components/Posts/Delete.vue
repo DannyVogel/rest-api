0f86753c402d4ad3a6fbb0ed51314e0069e5c120
Danny <danny.hj.vogel@gmail.com>
2024-06-17 00:27:28 +0200

    Feat: show live example with response

--- components/Posts/Delete.vue
+++ components/Posts/Delete.vue
@@ -14,6 +14,23 @@ const output = `{
 };`;
 
 const title = "Delete Post";
+const id = ref<number>(1);
+const isLoading = ref<boolean>(false);
+const response = ref();
+
+const deletePostById = async (id: number) => {
+  try {
+    isLoading.value = true;
+    const res = await $fetch(`/api/posts/${id}`, {
+      method: "DELETE",
+    });
+    response.value = res;
+  } catch (error: Response | any) {
+    console.log("error", error);
+  } finally {
+    isLoading.value = false;
+  }
+};
 </script>
 
 <template>
@@ -22,5 +39,23 @@ const title = "Delete Post";
     <Warn
       message="The updated post will not actually be deleted but the response will pretend it did."
     />
+    <p class="font-bold">Live Data Example:</p>
+    <UCard class="w-full min-h-60">
+      <div v-if="!response" class="flex gap-4">
+        <UInput v-model="id" type="number" color="gray" max="100" min="1" />
+        <UButton @click="deletePostById(id)" color="red" :loading="isLoading">
+          Delete Post with id: {{ id }}
+        </UButton>
+      </div>
+      <div v-else>
+        <pre>{{ response }}</pre>
+        <p
+          @click="response = null"
+          class="text-red-500 mt-4 cursor-pointer hover:text-red-700"
+        >
+          Delete another post
+        </p>
+      </div>
+    </UCard>
   </div>
 </template>
