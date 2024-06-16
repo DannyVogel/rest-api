425ad91f98048d3103038a6e79417522460e10e1
Danny <danny.hj.vogel@gmail.com>
2024-06-17 00:19:52 +0200

    Fix: remove homepage button

--- pages/index.vue
+++ pages/index.vue
@@ -22,11 +22,5 @@ const greet = async () => {
 <template>
   <div class="flex flex-col gap-4 items-center">
     <HomeWelcome />
-    <UButton @click="greet" size="xl">Click me</UButton>
-    <UCard v-if="message">
-      <div v-if="message" class="text-blue-500">
-        <p>{{ message }}</p>
-      </div>
-    </UCard>
   </div>
 </template>
