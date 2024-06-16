8600b704dd90025a185d7c18a070001905f045e6
Danny <danny.hj.vogel@gmail.com>
2024-06-17 00:01:57 +0200

    Style: fix icon size

--- components/Warn.vue
+++ components/Warn.vue
@@ -11,7 +11,7 @@ defineProps({
   <div class="flex gap-2 items-start sm:items-center">
     <UIcon
       name="i-heroicons-exclamation-triangle"
-      class="text-orange-500 mt-1.5 sm:mt-1 w-8"
+      class="text-orange-500 mt-1.5 sm:mt-1 w-4 h-4 shrink-0"
     />
     <p class="text-black dark:text-white">{{ message }}</p>
   </div>
